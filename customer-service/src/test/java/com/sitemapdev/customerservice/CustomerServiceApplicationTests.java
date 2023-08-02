5c24bdb377abb08b02a85443a39a4ababda87061
Kelvin M. Mwau [ICT] <kmwau@CO-OPBANK.CO.KE>
2023-08-02 21:11:59 +0300

    added customer-service

---
+++ customer-service/src/test/java/com/sitemapdev/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitemapdev.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
